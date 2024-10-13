ac07df5a7d31c080de033ac4889910b95f5f8232
Apalacios <Apalacios0020@gmail.com>
2024-10-12 20:06:49 -0600

    new hosting and correct linkedIn profile

--- lib/pages/base.dart
+++ lib/pages/base.dart
@@ -176,7 +176,7 @@ class _HomeState extends State<Home> {
                         IconButton(
                           onPressed: () {
                             controller.goToUrl(
-                                "https://www.linkedin.com/in/anthony-palacios");
+                                "https://www.linkedin.com/in/palacios-anthony");
                           },
                           icon: const FaIcon(
                             FontAwesomeIcons.linkedin,
